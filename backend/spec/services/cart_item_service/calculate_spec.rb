44f023ef89776ce0acc50499d591a57cc1ed893c
Dan Rey Oquindo <danreyoquindo@gmail.com>
2025-06-15 19:08:37 +0800

    Add specs

--- backend/spec/services/cart_item_service/calculate_spec.rb
+++ backend/spec/services/cart_item_service/calculate_spec.rb
@@ -6,15 +6,112 @@ RSpec.describe CartItemService::Calculate, type: :service do
 
     context 'product has one discount rule' do
       context 'product has buy X get Y' do
+        let(:product) { create(:product, name: 'Green Tea', price_in_cents: 311) }
+        let(:cart_item) { create(:cart_item, product: product, quantity: 21) }
+
+        before do
+          create(:buy_x_get_y_rule, buy: buy_count, get: get_count, product: product)
+        end
+
+        context 'buy 1 get 1' do
+          let(:buy_count) { 1 }
+          let(:get_count) { 1 }
+
+          it 'should return expected amount' do
+            # 21 products, 10 free . 11 x 311 
+            expect(subject.call).to eq(3421)
+          end
+        end
+
+        context 'buy 2 get 1' do
+          let(:buy_count) { 2 }
+          let(:get_count) { 1 }
+
+          it 'should return expected amount' do
+            # 21 products, 7 free. 14 x 311
+            expect(subject.call).to eq(4354)
+          end
+        end
+
+        context 'buy 3 get 2' do
+          let(:buy_count) { 3 }
+          let(:get_count) { 2 }
+
+          it 'should return expected amount' do
+            # 21 products, 8 free. 13 x 311
+            expect(subject.call).to eq(4043)
+          end
+        end
+
+        context 'buy 10 get 2' do
+          let(:buy_count) { 10 }
+          let(:get_count) { 2 }
+
+          it 'should return expected amount' do
+            # 21 products, 2 free. 19 x 311
+            expect(subject.call).to eq(5909)
+          end
+        end
       end
 
       context 'product is discounted when bulk' do
-        context 'discount is percentage based' do
+
+        context 'discount is amount specific' do
+          let(:product) { create(:product, name: 'Straweberry', price_in_cents: 500) }
+          let(:cart_item) { create(:cart_item, product: product, quantity: quantity) }
+  
+          before do
+            create(:amount_specific_rule, minimum: 5, new_price: 400, product: product)
+          end
+
+          context 'quantity is below minimum' do
+            let(:quantity) { 3 }
+
+            it 'should return expected amount' do
+              # 3 products, no discount. 3 x 500
+              expect(subject.call).to eq(1500)
+            end
+          end
+
+          context 'quantity is above minimum' do
+            let(:quantity) { 7 }
+
+            it 'should return expected amount' do
+              # 7 products, should use new price. 7 x 400
+              expect(subject.call).to eq(2800)
+            end
+          end
         end
 
-        context 'disocunt is amount specific based' do
+        context 'discount is percentage based' do
+          let(:product) { create(:product, name: 'Coffee', price_in_cents: 500) }
+          let(:cart_item) { create(:cart_item, product: product, quantity: quantity) }
+  
+          before do
+            create(:percentage_based_rule, minimum: 5, percentage: 0.66, product: product)
+          end
+
+          context 'quantity is below minimum' do
+            let(:quantity) { 3 }
+
+            it 'should return expected amount' do
+              # 3 products, no discount. 3 x 500
+              expect(subject.call).to eq(1500)
+            end
+          end
+
+          context 'quantity is above minimum' do
+            let(:quantity) { 7 }
+
+            it 'should return expected amount' do
+              # 7 products, should use (7 x 500) * 0.66
+              expect(subject.call).to eq(2310)
+            end
+          end
         end
       end
+
+
     end
 
     context 'product has multiple discount rule' do
