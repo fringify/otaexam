e3ae819d51cd5d9558c3ea6a34e0b55ecff60a53
Dan Rey Oquindo <danreyoquindo@gmail.com>
2025-06-15 17:38:05 +0800

    modify context to support multiple rules per product

--- backend/spec/services/cart_item_service/calculate_spec.rb
+++ backend/spec/services/cart_item_service/calculate_spec.rb
@@ -4,14 +4,24 @@ RSpec.describe CartItemService::Calculate, type: :service do
   describe '#call' do
     subject { described_class.new(cart_item).call }
 
-    context 'product has buy X get Y' do
-    end
-
-    context 'product is discounted when bulk' do
-      context 'discount is percentage based' do
+    context 'product has one discount rule' do
+      context 'product has buy X get Y' do
       end
 
-      context 'disocunt is amount specific based' do
+      context 'product is discounted when bulk' do
+        context 'discount is percentage based' do
+        end
+
+        context 'disocunt is amount specific based' do
+        end
+      end
+    end
+
+    context 'product has multiple discount rule' do
+      context 'combination of buy X get Y and percentage based' do
+      end
+
+      context 'combination of buy X get Y and amount specific' do
       end
     end
   end
